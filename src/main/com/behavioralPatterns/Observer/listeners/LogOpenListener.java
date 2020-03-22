d18a43f30ee183ee09523bce4dc46f897f6a4a79
Aliaksei Taran <aliakset@audible.com>
2020-03-21 21:42:00 -0400

    Implement Observer pattern

---
+++ src/main/com/behavioralPatterns/Observer/listeners/LogOpenListener.java
@@ -0,0 +1,18 @@
+package com.behavioralPatterns.Observer.listeners;
+
+import java.io.File;
+
+public class LogOpenListener implements EventListener {
+    private File log;
+
+    public LogOpenListener(String filename) {
+        this.log = new File(filename);
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log " + log +
+                ": Someone has performed " + eventType +
+                "operation with following file " + file.getName());
+    }
+}
